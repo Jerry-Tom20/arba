c0ea76511347655fb40e3d1ffe2f2f35fc1127d1
Jerry Thomas <jerry.thoma2000@gmail.com>
2023-05-12 16:05:31 +0530

    static_commit

--- arba/settings.py
+++ arba/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p$&e!6x(#6d*+3350xeoo*=io$6sp#_1jm7z=gid59qbu9zcte
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
