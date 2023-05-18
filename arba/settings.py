46d9f9c623550cc3af2ee8cd841203e96d347242
Jerry Thomas <jerry.thoma2000@gmail.com>
2023-05-18 20:43:08 +0530

    far

--- arba/settings.py
+++ arba/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-p$&e!6x(#6d*+3350xeoo*=io$6sp#_1jm7z=gid59qbu9zcte
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
